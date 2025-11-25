24bdca5427d9aa91765ccb713964c0d15e795dfd
Kamiltlusty <k.tlusty@student.uksw.edu.pl>
2025-11-25 18:31:46 +0100

    przed kolokwium

--- src/main/java/pl/kamil/Main.java
+++ src/main/java/pl/kamil/Main.java
@@ -104,6 +104,23 @@ public class Main {
         txtExp.save(ECDF, "ECDF_FUNCTION2_BINARY");
     }
 
+    public static void kolokwium(RandomNumbers rn) {
+        var dim = 10;
+        var execNum = 100;
+        var xMin = -32.768;
+        var xMax = 32.768;
+        Double optimum = 0.0;
+        final Map<Integer, List<Double>> fxResults = new TreeMap<>();
+        final Map<Integer, List<Double>> ECDF = new TreeMap<>();
+        DataExport txtExp = new TXTExport();
+        var ga = new GeneticAlgorithm(rn, new RepresentationConversionService(xMin, xMax));
+        boolean isBinary = true;
+
+        ga.runTask(dim, execNum, xMin, xMax, new TestFunc2(), fxResults, isBinary, optimum, ECDF);
+        txtExp.save(fxResults, "GENETIC_ALGORITHM_F2_BINARY");
+        txtExp.save(ECDF, "ECDF_FUNCTION2_BINARY");
+    }
+
     public static void main(String[] args) {
         var rn = new RandomlyGeneratedNumbers();
         lab4(rn);
---
+++ src/main/java/pl/kamil/domain/algorithm/Kolokwium.java
@@ -0,0 +1,195 @@
+package pl.kamil.domain.algorithm;
+
+import pl.kamil.domain.algorithm.sa.eval.func.EvalFunc;
+import pl.kamil.domain.model.Point;
+import pl.kamil.domain.model.UInt16;
+import pl.kamil.domain.service.RandomNumbers;
+import pl.kamil.domain.service.RepresentationConversionService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Kolokwium {
+    private final RandomNumbers rn;
+    private final RepresentationConversionService rcs;
+    private final int EVAL_FUNC_INVOKES = 10_000;
+    private final int POPULATION_SIZE = 100;
+
+    public Kolokwium(RandomNumbers rn, RepresentationConversionService rcs) {
+        this.rn = rn;
+        this.rcs = rcs;
+    }
+
+    public void runTask(Integer dim, int execNum, double xMin, double xMax, EvalFunc eFunc, Map<Integer, List<Double>> fxResults, boolean isBinary, Double optimum, Map<Integer, List<Double>> ECDF) {
+        double initialEval = Double.MAX_VALUE;
+        for (int i = 0; i < execNum; i++) {
+            executeAlgorithm(dim, xMin, xMax, eFunc, fxResults, initialEval, i, isBinary, optimum, ECDF);
+        }
+    }
+
+    private void executeAlgorithm(Integer dim, double xMin, double xMax,
+                                  EvalFunc eFunc, Map<Integer, List<Double>> fxResults,
+                                  double initialEval, int i, boolean isBinary,
+                                  Double optimum, Map<Integer, List<Double>> ECDF) {
+        List<Double> ecdfPerAlgorithm = new ArrayList<>();
+        List<Double> result = new ArrayList<>();
+        List<Point> population = new ArrayList<>();
+        // obliczenie poczatkowej sigmy ale do poczatkowej populacji nie trzeba jej zapisywac (nie uczestniczy w mutacji) dopiero do dzieci
+        double initialSigma = (xMax - xMin) * 0.05;
+
+        // generowanie populacji (punktów w dziedzinie) i wypelnienie wymiarow losowymi wartosciami
+        generatePopulation(dim, xMin, xMax, population);
+
+        // zastosowanie funkcji ewaluacji na populacji i utworznie mapy chromosom:ewaluacja
+        Map<Point, Double> populationEvaluation = evaluate(eFunc, population);
+
+        double eval = initialEval;
+        eval = addEvalToResult(populationEvaluation, eval, result, optimum, ecdfPerAlgorithm);
+
+        for (int j = 0; j < (EVAL_FUNC_INVOKES - POPULATION_SIZE) / POPULATION_SIZE; j++) {
+            //  turniej, w którym będzie t tur losowania l wartosci, z ktorych najlepsza zostanie wyselekcjonowana jako rodzic tworzac grupe rodzicow
+            // gdzie t to tyle co populacja, a l to liczba losowo wybranych wartosci
+            int t = POPULATION_SIZE;
+            int l = 2;
+            List<Point> parents = select(t, l, populationEvaluation, population);
+            // przed rekombinacja dla reprezentacji binarnej zmieniamy punkty na binarne
+            if (isBinary) {
+                parents.forEach(Point::toUInt16);
+            }
+
+            // rekombinacja
+            int k = 3;
+            List<Point> children = recombine(parents, k, dim);
+
+            // zapisanie sigmy do dzieci
+            children.forEach(p -> p.setSigma(initialSigma));
+            // mutacja
+            mutation(children, dim);
+            // po mutacji  dla reprezentacji binarnej zmieniamy punkty z binarnych na rzeczywistoliczbowe
+            if (isBinary) {
+                children.forEach(Point::fromUInt16toDomain);
+            }
+            // ewaluacja dzieci
+            Map<Point, Double> childrenEvaluation = evaluate(eFunc, children);
+            eval = addEvalToResult(childrenEvaluation, eval, result, optimum, ecdfPerAlgorithm);
+            // zamiana - wybieram lepszego z populacji rodziców (populationEvaluation) i dzieci i zapisuje do populationEvaluation
+            replace(population, populationEvaluation, children, childrenEvaluation);
+        }
+        fxResults.put(i, result);
+        ECDF.put(i, ecdfPerAlgorithm);
+    }
+
+    private static Map<Point, Double> evaluate(EvalFunc eFunc, List<Point> data) {
+        return data.stream().collect(Collectors.toMap(
+                key -> key,
+                eFunc::evalFunc));
+    }
+
+    private void generatePopulation(Integer dim, double xMin, double xMax, List<Point> population) {
+        List<Point> pointsTmp = Stream.generate(() -> new Point(rcs))
+                .limit(POPULATION_SIZE)
+                .toList();
+
+        pointsTmp.forEach(p -> p.fillCoordsWithRandValsFromDomain(dim, xMin, xMax));
+        population.addAll(pointsTmp);
+    }
+
+    private double addEvalToResult(Map<Point, Double> populationEvaluation, double eval, List<Double> result, Double optimum, List<Double> ecdfPerAlgorithm) {
+        for (var p : populationEvaluation.values()) {
+            double newEval = p;
+            eval = Math.min(eval, newEval);
+            result.add(eval);
+            ecdfPerAlgorithm.add(eval - optimum);
+        }
+        return eval;
+    }
+
+    private List<Point> select(int t, int l, Map<Point, Double> populationEvaluation, List<Point> population) {
+        List<Point> parents = new ArrayList<>();
+        for (int i = 0; i < t; i++) {
+            Point best = drawPointFromPopulation(population);
+            for (int j = 0; j < l; j++) {
+                Point drawnPoint = drawPointFromPopulation(population);
+                // minimalizacja
+                if (populationEvaluation.get(drawnPoint) <= populationEvaluation.get(best)) {
+                    best = drawnPoint;
+                }
+            }
+            parents.add(best);
+        }
+        return parents;
+    }
+
+    private Point drawPointFromPopulation(List<Point> population) {
+        return population.get(rn.nextInt(population.size()));
+    }
+
+    private List<Point> recombine(List<Point> parents, int k, int dim) {
+        List<Point> children = new ArrayList<>();
+        // z 100 rodziców losujemy k = 3 rodziców i wybieramy na zmiane ich wymiary tworzac dziecko i tak 100 razy i wychodzi nowa populacja
+        for (int i = 0; i < parents.size(); i++) {
+            List<Point> chosen = chooseK(parents, k);
+            Point child = new Point(rcs);
+            List<Double> dims = new ArrayList<>(dim);
+            for (int j = 0; j < dim; j++) {
+                dims.add(chosen.get(j % k).getCoords().get(j));
+            }
+            child.setCoords(dims);
+            children.add(child);
+        }
+        return children;
+    }
+
+
+    private List<Point> chooseK(List<Point> parents, int k) {
+        List<Point> chosen = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            chosen.add(parents.get(rn.nextInt(parents.size())));
+        }
+        return chosen;
+    }
+
+    private void mutation(List<Point> children, Integer dim) {
+        double epsilon0 = 1e-8;
+        double tau = 1 / Math.sqrt(dim);
+        // krok samo adaptacji
+        for (var child : children) {
+            double sigmaPrim = child.getSigma() * Math.exp(tau * rn.nextGaussian());
+            if (sigmaPrim < epsilon0) {
+                sigmaPrim = epsilon0;
+            }
+            child.setSigma(sigmaPrim);
+        }
+        // mutacja
+        for (var child : children) {
+            List<Double> newCoords = new ArrayList<>();
+            for (int i = 0; i < dim; i++) {
+                newCoords.add(child.getCoords().get(i) + rn.nextGaussian(0, child.getSigma()));
+            }
+            child.setCoords(newCoords);
+        }
+    }
+
+    private void replace(List<Point> population,
+                         Map<Point, Double> populationEvaluation,
+                         List<Point> children,
+                         Map<Point, Double> childrenEvaluation) {
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            Point parent = population.get(i);
+            Double parentFitness = populationEvaluation.get(parent);
+
+            Point child = children.get(i);
+            Double childFitness = childrenEvaluation.get(child);
+
+            // listy zapewniaja spojnosc rodzic dziecko
+            if (childFitness < parentFitness) {
+                population.set(i, child);
+                populationEvaluation.remove(parent);
+                populationEvaluation.put(child, childFitness);
+            }
+        }
+    }
+}
